78982e5b4fef9d96d12c63034d07fc83af837a4c
t.mesropyan <Tatul.Mesropyan-extern@invia.de>
2023-05-25 03:05:40 +0400

    New deps, eslint, prettier implementation

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
 import './App.css';
-import {useState} from "react";
-import {MenuItem, TableCell, TableRow, TextareaAutosize, TextField} from "@mui/material";
+import { useState } from 'react';
+import { MenuItem, TableCell, TableRow, TextareaAutosize, TextField } from '@mui/material';
 const mockData = [
-    {
-        id: 1,
-        name: 'John',
-        description: 'Lorem ipsum dolor sit amet',
-        date: '2023-05-20',
-        status: 'active',
-    },
-    {
-        id: 2,
-        name: 'Jane',
-        description: 'Consectetur adipiscing elit',
-        date: '2023-05-22',
-        status: 'pending',
-    },
-    {
-        id: 3,
-        name: 'Bob',
-        description: 'Sed do eiusmod tempor incididunt',
-        date: '2023-05-23',
-        status: 'canceled',
-    },
+  {
+    id: 1,
+    name: 'John',
+    description: 'Lorem ipsum dolor sit amet',
+    date: '2023-05-20',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Jane',
+    description: 'Consectetur adipiscing elit',
+    date: '2023-05-22',
+    status: 'pending'
+  },
+  {
+    id: 3,
+    name: 'Bob',
+    description: 'Sed do eiusmod tempor incididunt',
+    date: '2023-05-23',
+    status: 'canceled'
+  }
 ];
 function App() {
-  const [data, setData] = useState(mockData)
-  const [selectedRow, setSelectedRow]  = useState(null)
-  const handleFieldChange = (e,field,row) => {
-      selectedRow(row.id)
-      const updated = {...row ,[field]: e.target.value};
-      setData((prev) => (
-          prev?.map((item) =>  row.id === item.id ? {...item,...updated} : item)
-          )
-      )
-  }
-  console.log("Data", data)
+  const [data, setData] = useState(mockData);
+  const [selectedRow, setSelectedRow] = useState(null);
+  const handleFieldChange = (e, field, row) => {
+    const updated = { ...row, [field]: e.target.value };
+    setData((prev) => prev?.map((item) => (row.id === item.id ? { ...item, ...updated } : item)));
+  };
+  console.log('Data', data);
   return (
-      <table>
-          <thead>
+    <table>
+      <thead>
         <tr>
           <th>Id</th>
           <th>Name</th>
@@ -46,76 +42,71 @@ function App() {
           <th>Status</th>
           <th>Date</th>
         </tr>
-          </thead>
-          {data?.map((row, index) => (
-              <TableRow
-                  key={row.id}
-                  sx={{ cursor: 'pointer' }}
-                  onClick={() => setSelectedRow(row.id)}
+      </thead>
+      {data?.map((row, index) => (
+        <TableRow key={index} sx={{ cursor: 'pointer' }} onClick={() => setSelectedRow(row.id)}>
+          <TableCell>
+            {selectedRow === row.id ? (
+              <TextField
+                type="number"
+                value={row.id}
+                onChange={(e) => handleFieldChange(e, 'id', row)}
+              />
+            ) : (
+              row.id
+            )}
+          </TableCell>
+          <TableCell>
+            {selectedRow === row.id ? (
+              <TextField
+                type="text"
+                value={row.name}
+                onChange={(e) => handleFieldChange(e, 'name', row)}
+              />
+            ) : (
+              row.name
+            )}
+          </TableCell>
+          <TableCell>
+            {selectedRow === row.id ? (
+              <TextareaAutosize
+                value={row.description}
+                onChange={(e) => handleFieldChange(e, 'description', row)}
+              />
+            ) : (
+              row.description
+            )}
+          </TableCell>
+          <TableCell>
+            {selectedRow === row.id ? (
+              <TextField
+                type="date"
+                value={row.date}
+                onChange={(e) => handleFieldChange(e, 'date', row)}
+              />
+            ) : (
+              row.date
+            )}
+          </TableCell>
+          <TableCell>
+            {selectedRow === row.id ? (
+              <TextField
+                select
+                value={row.status}
+                onChange={(e) => handleFieldChange(e, 'status', row)}
               >
-                  <TableCell>
-                      {selectedRow === row.id ? (
-                          <TextField
-                              type="number"
-                              value={row.id}
-                              onChange={(e) => handleFieldChange(e, 'id', row)}
-                          />
-                      ) : (
-                          row.id
-                      )}
-                  </TableCell>
-                  <TableCell>
-                      {selectedRow === row.id ? (
-                          <TextField
-                              type="text"
-                              value={row.name}
-                              onChange={(e) => handleFieldChange(e, 'name', row)}
-                          />
-                      ) : (
-                          row.name
-                      )}
-                  </TableCell>
-                  <TableCell>
-                      {selectedRow === row.id ? (
-                          <TextareaAutosize
-                              value={row.description}
-                              onChange={(e) => handleFieldChange(e, 'description', row)}
-                          />
-                      ) : (
-                          row.description
-                      )}
-                  </TableCell>
-                  <TableCell>
-                      {selectedRow === row.id ? (
-                          <TextField
-                              type="date"
-                              value={row.date}
-                              onChange={(e) => handleFieldChange(e, 'date', row)}
-                          />
-                      ) : (
-                          row.date
-                      )}
-                  </TableCell>
-                  <TableCell>
-                      {selectedRow === row.id ? (
-                          <TextField
-                              select
-                              value={row.status}
-                              onChange={(e) => handleFieldChange(e, 'status', row)}
-                          >
-                              <MenuItem value="active">Active</MenuItem>
-                              <MenuItem value="pending">Pending</MenuItem>
-                              <MenuItem value="canceled">Canceled</MenuItem>
-                          </TextField>
-                      ) : (
-                          row.status
-                      )}
-                  </TableCell>
-                  <TableCell>
-                  </TableCell>
-              </TableRow>
-          ))}
-      </table>
+                <MenuItem value="active">Active</MenuItem>
+                <MenuItem value="pending">Pending</MenuItem>
+                <MenuItem value="canceled">Canceled</MenuItem>
+              </TextField>
+            ) : (
+              row.status
+            )}
+          </TableCell>
+          <TableCell></TableCell>
+        </TableRow>
+      ))}
+    </table>
   );
 }
 
