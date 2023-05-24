2999cf01c87451da728628725404c3d4bc9162b1
t.mesropyan <tatul.mesropyan@gmail.com>
2023-05-24 14:12:57 -0700

    first commit

---
+++ src/Row.jsx
@@ -0,0 +1,10 @@
+import {Grid} from '@mui/material';
+
+export const Table = () => {
+    return (
+        <Grid container spacing={2}>
+            <Grid item xl={5}>
+            </Grid>
+        </Grid>
+    )
+}
