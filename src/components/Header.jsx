ff52b3c5d26b2c5d3d9794fe7a3a7298872c1958
t.mesropyan <Tatul.Mesropyan-extern@invia.de>
2023-05-27 21:25:00 +0400

    Eslint fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Box, Paper } from '@mui/material';
+import { Paper } from '@mui/material';
 import React from 'react';
 import { TableCell } from './';
 export const Header = ({ titles, onQueryChange }) => {
